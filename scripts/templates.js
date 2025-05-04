fb6b10adadb923dd297240644d3c45a5c2f4880b
Henri Pallas <hrp1609a@tlu.ee>
2025-05-04 05:00:31 +0300

    feat: homepage desktop finished

--- scripts/templates.js
+++ scripts/templates.js
@@ -10,6 +10,11 @@ const loginButtonFile = partials + "/login_button.hbs";
 const mainContentFile = partials + "/main_content.hbs";
 const carousellFile = partials + "/carousell.hbs";
 const cardFile = partials + "/card.hbs"
+const cardEventsFile = partials + "/card_events.hbs"
+const cardImportantFile = partials + "/card_important.hbs"
+const cardContactsFile = partials + "/card_contacts.hbs"
+const cardCrisisFile = partials + "/card_crisis.hbs"
+const cardFormsFile = partials + "/card_forms.hbs"
 const footerFile = partials + "/footer.hbs";
 
 const mainTemplate = $("#main-page").html();
@@ -26,8 +31,27 @@ $(function() {
         $.get(mainContentFile),
         $.get(carousellFile),
         $.get(cardFile),
+        $.get(cardEventsFile),
+        $.get(cardImportantFile),
+        $.get(cardContactsFile),
+        $.get(cardCrisisFile),
+        $.get(cardFormsFile),
         $.get(footerFile),
-    ).done(function(headerTemplate, searchTemplate, navTemplate, loginButtonFile, mainContentTemplate, carousellTemplate, cardTemplate, footerTemplate) {
+    ).done(function(
+        headerTemplate, 
+        searchTemplate, 
+        navTemplate, 
+        loginButtonFile, 
+        mainContentTemplate, 
+        carousellTemplate, 
+        cardTemplate, 
+        cardEventsTemplate, 
+        cardImportantTemplate,
+        cardContactsTemplate,
+        cardCrisisTemplate,
+        cardFormsTemplate,
+        footerTemplate
+    ) {
         Handlebars.registerPartial('header', headerTemplate[0]);
         Handlebars.registerPartial('search', searchTemplate[0]);
         Handlebars.registerPartial('nav', navTemplate[0]);
@@ -35,6 +59,11 @@ $(function() {
         Handlebars.registerPartial('main_content', mainContentTemplate[0]);
         Handlebars.registerPartial('carousell', carousellTemplate[0]);
         Handlebars.registerPartial('card', cardTemplate[0]);
+        Handlebars.registerPartial('card_events', cardEventsTemplate[0]);
+        Handlebars.registerPartial('card_important', cardImportantTemplate[0]);
+        Handlebars.registerPartial('card_contacts', cardContactsTemplate[0]);
+        Handlebars.registerPartial('card_crisis', cardCrisisTemplate[0]);
+        Handlebars.registerPartial('card_forms', cardFormsTemplate[0]);
         Handlebars.registerPartial('footer', footerTemplate[0]);
 
         var compiledMainTemplate = Handlebars.compile(mainTemplate);
